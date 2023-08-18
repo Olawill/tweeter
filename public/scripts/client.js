b30f73df9740c436e4588e69c80c1801174de477
Olawale <henry.williams658@gmail.com>
2023-08-18 05:12:03 +0000

    Fix counter bug and refactor the code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,33 @@
  */
 
 
+// LOAD THE TWEET ELEMENTS ON THE PAGE
+const createTweetElement = (tweetObject) => {
+
+  let $tweet =  `<article class="tweets__item">
+            <header>
+              <div>
+                <span>
+                  <img src="${tweetObject.user.avatars}" alt="${tweetObject.user.name}">
+                  <span>${tweetObject.user.name}</span>
+                </span>
+                <a href="#">${tweetObject.user.handle}</a>
+              </div>
+              <div>${escaped(tweetObject.content.text)}</div>
+            </header>
+            <hr>
+            <footer>
+              <div class="tweets__item__posted">${timeago.format(new Date(tweetObject.created_at))}</div>
+              <div class="icons">
+                <i class="fa-solid fa-flag"></i>
+                <i class="fa-solid fa-retweet"></i>
+                <i class="fa-solid fa-heart"></i>
+              </div>
+            </footer>
+          </article>`;
+  return $tweet;
+};
+
 // LOOP FOR THE RENDERING TWEETS ON THE PAGE
 const renderTweets = (tweets) => {
 
@@ -25,32 +52,6 @@ const escaped = function (str) {
 }
 
 
-// LOAD THE TWEET ELEMENTS ON THE PAGE
-const createTweetElement = (tweetObject) => {
-
-  let $tweet =  `<article class="tweets__item">
-            <header>
-              <div>
-                <span>
-                  <img src="${escaped(tweetObject.user.avatars)}" alt="${escaped(tweetObject.user.name)}">
-                  <span>${escaped(tweetObject.user.name)}</span>
-                </span>
-                <a href="#">${escaped(tweetObject.user.handle)}</a>
-              </div>
-              <div>${escaped(tweetObject.content.text)}</div>
-            </header>
-            <hr>
-            <footer>
-              <div class="tweets__item__posted">${escaped(timeago.format(new Date(tweetObject.created_at)))}</div>
-              <div class="icons">
-                <i class="fa-solid fa-flag"></i>
-                <i class="fa-solid fa-retweet"></i>
-                <i class="fa-solid fa-heart"></i>
-              </div>
-            </footer>
-          </article>`;
-  return $tweet;
-};
 
 
 // Ensure DOM is loaded
@@ -83,12 +84,15 @@ $(() => {
         type: "POST",
         url: 'tweets',
         data: data,
-        success: function(data) {
-            $.ajax('http://localhost:8080/tweets', { method: 'GET' })
+        success: function() {
+            $.ajax('/tweets', { method: 'GET' })
               .then(function (data) {
                 const newTweet = data.slice(-1)[0];
                 $('#tweets-container').prepend(createTweetElement(newTweet));
               });
+            },
+        error: function(err) {
+          console.log(`Error posting to server ${JSON.stringify(err)}`);
             }
           })
         }
@@ -116,12 +120,12 @@ $(() => {
         $('.error-message').slideDown(400, () => {
           $(this)
           .css(
-            'display', 'flex'
+              'display', 'flex'
           );
         });
     } else {
       $('#error-text').text('');
-        $('.error-message').slideUp("slow", () => {
+        $('.error-message').slideUp("fast", () => {
           $(this)
           .css(
             'display', 'none'
@@ -132,11 +136,12 @@ $(() => {
 
   // LOAD TWEET ON TO THE PAGE
   const $loadTweet = () => {
-    $.ajax('http://localhost:8080/tweets', { method: 'GET' })
+    $.ajax('/tweets', { method: 'GET' })
     .then(function (data) {
-      // console.log('Success: ', data);
       renderTweets(data);
-
+    })
+    .catch((err) => {
+      console.log(`Error loading tweets ${err}`);
     });
   };
   $loadTweet();
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,38 +1,32 @@
 $(document).ready(function() {
   // --- our code goes here ---
 
-  const inputLength = [];
-  
+
+  // Extract the number of text input available
+  const $wordsLeft = Number($('.counter').val());
+
   $("#tweet-text").on('input', function() {
     // Element Definition::
     const $counterElement = $(this).siblings('div').children('.counter');
 
     // Get the length of the inputted text
-    const $message = $(this).val().length;
-
-    // Store the lengths in a lengths array
-    inputLength.push($message);
-
-    // Extract the number of text input available
-    const wordsLeft = Number($counterElement.text());
+    const $messageLength = $(this).val().length;
 
     /**
      * Check Length of Input length array
      * Less than or Equal to 1 => input pasted in or just one letter
      * More than 1 ==> input enter by typing one character at a time
-     * */
-    if (inputLength.length <= 1) {
-        $counterElement.text(wordsLeft - inputLength.slice(-1));
-    } else {
-        $counterElement.text(wordsLeft - inputLength.slice(-2)[1] + inputLength.slice(-2)[0]);
-    }
+     * Reduce the character counter  
+     * 
+     */
+    $counterElement.val($wordsLeft - $messageLength);
 
     // Ensure that color of counter is black
     $counterElement.css('color', 'black');
 
     // When counter is less than or equal to zero, change color to red
-    if (Number($counterElement.text()) <= 0) {
+    if (Number($counterElement.val()) <= 0) {
         $counterElement.css('color', 'red'); 
-    } 
- });
+    }
+  });
 });
